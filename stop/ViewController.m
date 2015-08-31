974582a016aa71cd5eb469dab56282e0122dbb5a
sathish chinniah <sathish73ec@gmail.com>
2015-08-31 19:33:06 +0530

    initial commit

--- stop/ViewController.m
+++ stop/ViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+
+#import <AVFoundation/AVFoundation.h>
+
 
 @interface ViewController ()
+
+
+@property (nonatomic, strong) AVAudioPlayer *audioPlayer;
+- (IBAction)playSound:(id)sender;
+- (IBAction)stopSound:(id)sender;
 
 @end
 
@@ -17,11 +26,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSBundle *mainBundle = [NSBundle mainBundle];
+    
+    NSString *filePath = [mainBundle pathForResource:@"(ABCD)_-_Sadda_Dil_Vi_Tu_(Ga_Ga_Ga_Ganpati)_Official_New_HD_Full_Song_V" ofType:@"mp3"];
+    
+    NSData *fileData = [NSData dataWithContentsOfFile:filePath];
+    
+    NSError *error = nil;
+    self.audioPlayer = [[AVAudioPlayer alloc] initWithData:fileData  error:&error];
+    
+    [self.audioPlayer prepareToPlay];
+    self.audioPlayer.numberOfLoops = -1;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-
+(IBAction)playSound:(id)sender {
+    [self.audioPlayer play];
+    
+    UIView *wrapperView = [[UIView alloc] initWithFrame:CGRectMake(20, 120, 260, 20)];
+    wrapperView.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:wrapperView];
+    
+    MPVolumeView *volumeView = [[MPVolumeView alloc] initWithFrame: wrapperView.bounds];
+    [wrapperView addSubview:volumeView];
+}
+- (IBAction)stopSound:(id)sender {
+    
+     [self.audioPlayer stop];
+    
 }
 
 @end
